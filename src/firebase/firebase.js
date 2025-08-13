331fa2aecdc534210f1f0b2f1d53654ef3d52973
Gaurav Singh <137911023+gauravsinghiitb@users.noreply.github.com>
2025-08-14 00:57:56 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,16 +3,7 @@ import { getFirestore, doc, getDoc, setDoc, onSnapshot, collection, getDocs, upd
 import { getStorage } from 'firebase/storage';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAMmdwsDZMZ5DJqGAHa4p6alcHY0-qdpZ4",
-  authDomain: "ai-media-platform.firebaseapp.com",
-  databaseURL: "https://ai-media-platform-default-rtdb.firebaseio.com",
-  projectId: "ai-media-platform",
-  storageBucket: "ai-media-platform.firebasestorage.app",
-  messagingSenderId: "729899008110",
-  appId: "1:729899008110:web:e98ee36968814dae78267f",
-  measurementId: "G-N7CQWMT1V6"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
